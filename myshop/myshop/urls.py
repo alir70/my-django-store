bce6842190ecc2eb5da602330b2c510280407d32
ali <ali.rozbehi@gmail.com>
2025-05-18 00:31:48 +0330

    added order system

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('', include('store.urls')),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
+
 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
